f97ee9008122a51f8d5028214e7b2e7be3bf3f5d
John-Tomo <johntomowest2@gmail.com>
2023-12-04 04:04:17 +0000

    first commit modified

--- brite.sh
+++ brite.sh
@@ -4,11 +4,15 @@
 # Big Jonn doesnt fall in love that easily
 
 echo "are you in love?"
-read answer1
-echo "if $answer1 then whats here name"
+read youranswer
+echo "if $youranswer then whats here name"
 read name
 echo "what do you love about $name"
 read fav
 echo "if $fav then she should be the perfect match"
 echo "i wish you both the best of luck if it ever happens"
-echo "Bye"
+echo "Do you have a message for $name?"
+read answer2
+echo "please type in your message"
+read message
+echo your message has been sent to $name
