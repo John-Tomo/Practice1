d3de3f8d3236e8d974c9587c68c828d6c54d6a90
John-Tomo <johntomowest2@gmail.com>
2023-12-04 03:39:11 +0000

    my first commit

---
+++ brite.sh
@@ -0,0 +1,14 @@
+# My first scritp
+# Dedicated to someone i have fallen in love with
+#hmmmm... I know right?
+# Big Jonn doesnt fall in love that easily
+
+echo "are you in love?"
+read answer1
+echo "if $answer1 then whats here name"
+read name
+echo "what do you love about $name"
+read fav
+echo "if $fav then she should be the perfect match"
+echo "i wish you both the best of luck if it ever happens"
+echo "Bye"
